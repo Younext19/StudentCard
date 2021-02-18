677b298e1b50ed9d5e260e162781c25eb7261267
Younext <youneshaddam9@gmail.com>
2021-02-18 13:27:53 +0100

    form Proj

--- src/formproj/FormProj.java
+++ src/formproj/FormProj.java
@@ -1,21 +1,263 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package formproj;
 
-/**
- *
- * @author Dams
- */
-public class FormProj {
+import java.awt.*; 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*; 
 
-    /**
-     * @param args the command line arguments
-     */
+public class FormProj extends JFrame implements ActionListener{
+
+   // Components of the Form 
+    ImageIcon icon;
+     Container c; 
+     JLabel title; 
+     JLabel name; 
+     JTextField tname; 
+     JLabel mno; 
+     JTextField tmno; 
+     JLabel gender; 
+     JRadioButton male; 
+     JRadioButton female; 
+     ButtonGroup gengp; 
+     JLabel dob; 
+     JComboBox date; 
+     JComboBox month; 
+     JComboBox year; 
+     JLabel add; 
+     JTextArea tadd; 
+     JCheckBox term; 
+     JButton sub; 
+     JButton reset; 
+     JTextArea tout; 
+     JLabel res; 
+     JTextArea resadd; 
+  Image backgroundImage;
+     String dates[] 
+       = { "1", "2", "3", "4", "5", 
+            "6", "7", "8", "9", "10", 
+            "11", "12", "13", "14", "15", 
+            "16", "17", "18", "19", "20", 
+            "21", "22", "23", "24", "25", 
+            "26", "27", "28", "29", "30", 
+            "31" }; 
+    private String months[] 
+        = { "Janvier", "fevrier", "Mar", "Avril", 
+            "Mai", "Juin", "Juillet", "Aout", 
+            "Septembre", "Octobre", "Novembre", "Decembre" }; 
+    private String years[] 
+        = { "1995", "1996", "1997", "1998", 
+            "1999", "2000", "2001" }; 
+        static JLabel l; 
+
+    public FormProj()
+    {
+
+        setTitle("Inscription"); 
+        setBounds(300, 90, 900, 600); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE); 
+        setResizable(false); 
+        
+        c = getContentPane(); 
+        c.setLayout(null); 
+
+      
+        
+
+        title = new JLabel("Inscription"); 
+        title.setFont(new Font("Arial", Font.PLAIN, 30)); 
+        title.setSize(300, 30); 
+        title.setLocation(300, 30); 
+        title.setOpaque(true);
+        c.add(title); 
+  
+        name = new JLabel("Nom :"); 
+        name.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        name.setSize(100, 20); 
+        name.setLocation(100, 100); 
+        c.add(name); 
+  
+        tname = new JTextField(); 
+        tname.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        tname.setSize(190, 20); 
+        tname.setLocation(200, 100); 
+        c.add(tname); 
+  
+        mno = new JLabel("Tel"); 
+        mno.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        mno.setSize(100, 20); 
+        mno.setLocation(100, 150); 
+        c.add(mno);
+  
+        tmno = new JTextField(); 
+        tmno.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        tmno.setSize(150, 20); 
+        tmno.setLocation(200, 150); 
+        c.add(tmno); 
+  
+        gender = new JLabel("Sexe"); 
+        gender.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        gender.setSize(100, 20); 
+        gender.setLocation(100, 200); 
+        c.add(gender); 
+  
+        male = new JRadioButton("Homme"); 
+        male.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        male.setSelected(true); 
+        male.setSize(75, 20); 
+        male.setLocation(200, 200); 
+        c.add(male); 
+  
+        female = new JRadioButton("Femme"); 
+        female.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        female.setSelected(false); 
+        female.setSize(80, 20); 
+        female.setLocation(275, 200); 
+        c.add(female); 
+  
+        gengp = new ButtonGroup(); 
+        gengp.add(male); 
+        gengp.add(female); 
+  
+        dob = new JLabel("Date De Naissance"); 
+        dob.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        dob.setSize(100, 20); 
+        dob.setLocation(100, 250); 
+        c.add(dob); 
+  
+        date = new JComboBox(dates); 
+        date.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        date.setSize(50, 20); 
+        date.setLocation(200, 250); 
+        c.add(date); 
+  
+        month = new JComboBox(months); 
+        month.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        month.setSize(60, 20); 
+        month.setLocation(250, 250); 
+        c.add(month); 
+  
+        year = new JComboBox(years); 
+        year.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        year.setSize(60, 20); 
+        year.setLocation(320, 250); 
+        c.add(year); 
+  
+        add = new JLabel("Adresse"); 
+        add.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        add.setSize(100, 20); 
+        add.setLocation(100, 300); 
+        c.add(add); 
+  
+        tadd = new JTextArea(); 
+        tadd.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        tadd.setSize(200, 75); 
+        tadd.setLocation(200, 300); 
+        tadd.setLineWrap(true); 
+        c.add(tadd); 
+  
+        term = new JCheckBox("Accepter les conditions"); 
+        term.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        term.setSize(250, 20); 
+        term.setLocation(150, 400); 
+        c.add(term); 
+  
+        sub = new JButton("Enregistrer"); 
+        sub.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        sub.setSize(100, 20); 
+        sub.setLocation(150, 450); 
+        sub.addActionListener(this); 
+
+        c.add(sub); 
+  
+        reset = new JButton("Reset"); 
+        reset.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        reset.setSize(100, 20); 
+        reset.setLocation(270, 450);
+        reset.addActionListener(this);
+        c.add(reset); 
+  
+        tout = new JTextArea(); 
+        tout.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        tout.setSize(300, 400); 
+        tout.setLocation(500, 100); 
+        tout.setLineWrap(true); 
+        tout.setEditable(false); 
+        c.add(tout); 
+  
+        res = new JLabel(""); 
+        res.setFont(new Font("Arial", Font.PLAIN, 20)); 
+        res.setSize(500, 25); 
+        res.setLocation(100, 500); 
+        c.add(res); 
+  
+        
+        resadd = new JTextArea(); 
+        resadd.setFont(new Font("Arial", Font.PLAIN, 15)); 
+        resadd.setSize(200, 75); 
+        resadd.setLocation(580, 175); 
+        resadd.setLineWrap(true); 
+        c.add(resadd); 
+  
+        setVisible(true); 
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        // create a frame 
+        SwingUtilities.invokeLater(FormProj::new);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+if (e.getSource() == sub) { 
+            if (term.isSelected()) { 
+                String data1; 
+                String data 
+                    = "Name : "
+                      + tname.getText() + "\n"
+                      + "Mobile : "
+                      + tmno.getText() + "\n"; 
+                if (male.isSelected()) 
+                    data1 = "Gender : Male"
+                            + "\n"; 
+                else
+                    data1 = "Gender : Female"
+                            + "\n"; 
+                String data2 
+                    = "DOB : "
+                      + (String)date.getSelectedItem() 
+                      + "/" + (String)month.getSelectedItem() 
+                      + "/" + (String)year.getSelectedItem() 
+                      + "\n"; 
+  
+                String data3 = "Address : " + tadd.getText(); 
+                tout.setText(data + data1 + data2 + data3); 
+                tout.setEditable(false); 
+                res.setText("Registration Successfully.."); 
+            } 
+            else { 
+                tout.setText(""); 
+                resadd.setText(""); 
+                res.setText("Please accept the"
+                            + " terms & conditions.."); 
+            } 
+        } 
+  
+        else if (e.getSource() == reset) { 
+            String def = ""; 
+            tname.setText(def); 
+            tadd.setText(def); 
+            tmno.setText(def); 
+            res.setText(def); 
+            tout.setText(def); 
+            term.setSelected(false); 
+            date.setSelectedIndex(0); 
+            month.setSelectedIndex(0); 
+            year.setSelectedIndex(0); 
+            resadd.setText(def); 
+        }   
     }
     
 }
